e186f02f481fa97a67d7427b658dcf4a49c8a781
Syed_Daniyal-14 <syeddaniyalakhtar4810@gmail.com>
2024-06-19 11:25:43 +0500

    first commit

---
+++ Assignment 5/main.js
@@ -0,0 +1,9 @@
+//Assignment number: 5
+//Store famous person's name in a variable
+var famousPerson = "Quaid-e-Azam Muhammad Ali Jinah";
+//Store the qoute in a variable
+var qoute = "\"I don't believe in taking the right desicion, I take a desicion and make it right\"";
+//Now store your message in a new variable
+var message = "".concat(famousPerson, ", Once said : ").concat(qoute);
+//Print your message
+console.log(message);
---
+++ Assignment 5/main.ts
@@ -0,0 +1,18 @@
+//Assignment number: 5
+
+
+//Store famous person's name in a variable
+
+let famousPerson:string = "Quaid-e-Azam Muhammad Ali Jinah";
+
+//Store the qoute in a variable
+
+let qoute: string = `"I don't believe in taking the right desicion, I take a desicion and make it right"`;
+
+//Now store your message in a new variable
+
+let message: string= `${famousPerson}, Once said : ${qoute}`
+
+//Print your message
+
+console.log (message);
