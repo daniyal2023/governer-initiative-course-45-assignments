60d992194b2d2e36766886657cbdd56d9ff80fcc
Syed_Daniyal-14 <147078345+daniyal2023@users.noreply.github.com>
2024-06-19 19:23:29 +0500

    Delete Assignment 1 directory

--- Assignment 1/main.ts
+++
@@ -1,9 +0,0 @@
-// Assignment # 2
-
-//Storing a persons name in a variable
-
-let personName:string = "Daniyal" ;
-
-// Printing a message to person
-
-console.log(`Hello ${personName} how are you? Would you like to learn some Typescript today?`);
