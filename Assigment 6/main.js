212125ac75ad9c7e557e406365d5bdc132ea9b78
Syed_Daniyal-14 <syeddaniyalakhtar4810@gmail.com>
2024-06-19 19:44:01 +0500

    first commit

---
+++ Assigment 6/main.js
@@ -0,0 +1,8 @@
+"use strict";
+//Assignment # 6
+//name with white-spaces
+let personName = "\t\n Daniyal \n\t";
+console.log("Name with white-space : ", personName);
+//Stripping white spaces
+let strippedName = personName.trim();
+console.log("Name with stripped whitespaces : ", strippedName);
---
+++ Assigment 6/main.ts
@@ -0,0 +1,11 @@
+//Assignment # 6
+
+//name with white-spaces
+
+let personName : string = "\t\n Daniyal \n\t";
+console.log("Name with white-space : ", personName);
+
+//Stripping white spaces
+
+let strippedName: string = personName.trim();
+console.log("Name with stripped whitespaces : ", strippedName);
