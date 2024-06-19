d1f64f99e390a0492c63bfbafc5eff38637a0483
Syed_Daniyal-14 <syeddaniyalakhtar4810@gmail.com>
2024-06-19 17:45:58 +0500

    renamed folder and changed first line of code

---
+++ Assignment 4/main.ts
@@ -0,0 +1,11 @@
+// Assignment # 4
+
+//Storing qoute and its author's name in a variable
+
+let qoute:string = "Setting goals is the first step in turning the invisible into the visible ";
+
+let authorName:string = "Tony robbins";
+
+//Printing the message
+
+console.log(`${authorName} said: "${qoute}"`);
