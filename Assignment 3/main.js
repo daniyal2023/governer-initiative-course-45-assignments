4256d875b22c4791260d342bbbb99647f97e6650
Syed_Daniyal-14 <147078345+daniyal2023@users.noreply.github.com>
2024-06-19 17:53:17 +0500

    Delete Assignment 3 directory
    I delete this directory because I have reuploaded this directory by changing it's name

--- Assignment 3/main.js
+++
@@ -1,6 +0,0 @@
-// Assignment # 3
-//Storing qoute and its author's name in a variable
-var qoute = "Setting goals is the first step in turning the invisible into the visible ";
-var authorName = "Tony robbins";
-//Printing the message
-console.log("".concat(authorName, " said: \"").concat(qoute, "\""));
