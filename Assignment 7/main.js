500ba96cf4305bf9fb3b786f6b784c8815a4582e
Syed_Daniyal-14 <syeddaniyalakhtar4810@gmail.com>
2024-06-24 17:04:03 +0500

    first commit

---
+++ Assignment 7/main.js
@@ -0,0 +1,14 @@
+"use strict";
+//Assignment 7 and 8
+//Addition
+let additionResult = 7 + 1;
+console.log("Addition result : ", additionResult);
+//Subtraction
+let subtractionResult = 13 - 5;
+console.log("Subtraction : ", subtractionResult);
+//Multiplication
+let multiplicationResult = 2 * 4;
+console.log("Multiplication : ", multiplicationResult);
+//Division
+let divisionResult = 32 / 4;
+console.log("Division : ", divisionResult);
---
+++ Assignment 7/main.ts
@@ -0,0 +1,18 @@
+//Assignment no. 7 
+
+//Addition
+let additionResult: number = 7 + 1;
+console.log("Addition result : " , additionResult);
+
+//Subtraction
+let subtractionResult: number = 13 - 5;
+console.log("Subtraction : " , subtractionResult);
+
+//Multiplication
+let multiplicationResult:number = 2 * 4;
+console.log("Multiplication : " , multiplicationResult);
+
+
+//Division
+let divisionResult : number = 32 / 4;
+console.log("Division : " , divisionResult);
