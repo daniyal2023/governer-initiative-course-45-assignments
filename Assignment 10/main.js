cf76ebdff781a5fb561e793bbe196880a62829c5
Syed_Daniyal-14 <syeddaniyalakhtar4810@gmail.com>
2024-06-24 17:35:06 +0500

    first commit

---
+++ Assignment 10/main.js
@@ -0,0 +1,12 @@
+"use strict";
+//Assignment no. 10
+//Syed Daniyal Akhtar
+// 24 / june / 2024
+//This program Subtracts two numbers and prints the result
+//Define two numbers
+let number1 = 19;
+let number2 = 7;
+//Subtract the numbers
+let subtractionResult = number1 - number2;
+//print the result
+console.log("Result :", subtractionResult);
---
+++ Assignment 10/main.ts
@@ -0,0 +1,16 @@
+//Assignment no. 10
+
+//Syed Daniyal Akhtar
+// 24 / june / 2024
+
+//This program Subtracts two numbers and prints the result
+
+//Define two numbers
+let number1: number = 19;
+let number2 : number =  7;
+
+//Subtract the numbers
+let subtractionResult: number = number1 - number2;
+
+//print the result
+console.log("Result :" , subtractionResult);
