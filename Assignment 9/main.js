d99fa06d01d02475ba93bdcd912d15eb5dc5daa0
Syed_Daniyal-14 <syeddaniyalakhtar4810@gmail.com>
2024-06-24 17:17:03 +0500

    first commit

---
+++ Assignment 9/main.js
@@ -0,0 +1,8 @@
+"use strict";
+//Assignment no. 9
+//Favorite Number
+let favoriteNumber = 14;
+//message
+let message = `My favorite number is : ${favoriteNumber}.`;
+//printing messsage
+console.log(message);
---
+++ Assignment 9/main.ts
@@ -0,0 +1,10 @@
+//Assignment no. 9
+
+//Favorite Number
+let favoriteNumber: number = 14;
+
+//message
+let message:string = `My favorite number is : ${favoriteNumber}.`;
+
+//printing messsage
+console.log(message);
