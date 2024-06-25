57c5601c1c45eaab3de64639acb73adc3efb4281
Syed_Daniyal-14 <syeddaniyalakhtar4810@gmail.com>
2024-06-25 21:35:45 +0500

    first commit

---
+++ Assignment 11/main.ts
@@ -0,0 +1,12 @@
+//Assignment 11
+
+//create a array called names and store some names in it
+
+let names : string[] = [ "Ibrahim" , "Muneeb" , "Muzammil" , "Mubashir" , "Mudassir" ] ;
+
+//Now print each name using for loop
+
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i])
+};
+
