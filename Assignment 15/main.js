995940e406719ce37b63d8d17cc79cceac7589db
Syed_Daniyal-14 <syeddaniyalakhtar4810@gmail.com>
2024-06-26 21:50:21 +0500

    first commit

---
+++ Assignment 15/main.js
@@ -0,0 +1,15 @@
+"use strict";
+//Assignment no. 15
+//continue with the array of exercise 14
+let guestNames = ["Aliyan", "Rafay", "Muhammad", "Kashan", "Musab", "Maaz"];
+//store the guest name which could not attend the dinner in a new variable
+let cannotAttend = "Muhammad";
+console.log(`${cannotAttend}, connot attend dinner tommorow.`);
+//Now add a new guest
+let newGuest = "Rohan";
+//now replace the previous guest with the new one using indexOf()
+guestNames[guestNames.indexOf(cannotAttend)] = newGuest;
+//print the names with the invitation message
+guestNames.forEach(guests => {
+    console.log(`Asallam o alaikum ! ${guests} would you like to come to my house for dinner tommorow ?`);
+});
