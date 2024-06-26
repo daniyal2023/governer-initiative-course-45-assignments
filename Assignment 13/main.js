7accdc9473eef04cb0ce98226303a0a0793b97c7
Syed_Daniyal-14 <syeddaniyalakhtar4810@gmail.com>
2024-06-26 14:44:48 +0500

    first commit

---
+++ Assignment 13/main.js
@@ -0,0 +1,9 @@
+"use strict";
+//Assignment no.13
+//create a array and storre some elements in it 
+let favoriteTransport = ["Mercedes G wagon", "Hyundai Sonata", "Yamaha YBR 125", "Kia Sportage"];
+//print the statement in form of list usin for of loop
+for (const Transport of favoriteTransport) {
+    console.log(`I would like to own a, ${Transport}.`);
+}
+;
---
+++ Assignment 13/main.ts
@@ -0,0 +1,10 @@
+//Assignment no.13
+
+//create a array and storre some elements in it 
+let favoriteTransport : string [] = ["Mercedes G wagon" , "Hyundai Sonata" , "Yamaha YBR 125" , "Kia Sportage"];
+
+//print the statement in form of list usin for of loop
+
+for (const Transport of favoriteTransport) {
+    console.log(`I would like to own a, ${Transport}.`)
+};
