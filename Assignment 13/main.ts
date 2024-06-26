85dcaad23e743fdb8f74656fed41902590a7d976
Syed_Daniyal-14 <syeddaniyalakhtar4810@gmail.com>
2024-06-26 14:49:05 +0500

    first commit

--- Assignment 13/main.ts
+++ Assignment 13/main.ts
@@ -1,6 +1,6 @@
 //Assignment no.13
 
-//create a array and storre some elements in it 
+//create a array and store some elements in it 
 let favoriteTransport : string [] = ["Mercedes G wagon" , "Hyundai Sonata" , "Yamaha YBR 125" , "Kia Sportage"];
 
 //print the statement in form of list usin for of loop
