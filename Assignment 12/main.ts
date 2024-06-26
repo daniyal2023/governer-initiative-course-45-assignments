d784793d4275cc3ab586c592c4f65e35f737c21e
Syed_Daniyal-14 <syeddaniyalakhtar4810@gmail.com>
2024-06-26 14:11:34 +0500

    First commit

---
+++ Assignment 12/main.ts
@@ -0,0 +1,11 @@
+//Assignment no. 12
+
+//create a array called names and store some names in it
+
+let names : string[] = [ "Ibrahim" , "Muneeb" , "Muzammil" , "Mubashir" , "Mudassir" ] ;
+
+//Now print each name and your message using for loop
+
+for (let i = 0; i < names.length; i++) {
+    console.log("Asallam o Alaikum ", names[i], ", would you like to learn some Typescript ?")
+};
