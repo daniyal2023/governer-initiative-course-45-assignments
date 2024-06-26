c0140943fb261a83d257592a052eb315712083f9
Syed_Daniyal-14 <syeddaniyalakhtar4810@gmail.com>
2024-06-26 20:41:21 +0500

    first commit

---
+++ Assignment 14/main.js
@@ -0,0 +1,8 @@
+"use strict";
+//Assignment no. 14
+//create an array of guests
+let guestNames = ["Aliyan", "Rafay", "Muhammad", "Kashan", "Musab", "Maaz"];
+//print the names with the invitation message
+guestNames.forEach(guests => {
+    console.log(`Asallam o alaikum ! ${guests} would you like to come to my house for dinner tommorow ?`);
+});
---
+++ Assignment 14/main.ts
@@ -0,0 +1,10 @@
+//Assignment no. 14
+
+//create an array of guests
+let guestNames: string[] = ["Aliyan" , "Rafay" , "Muhammad" , "Kashan" , "Musab" , "Maaz"];
+
+//print the names with the invitation message
+guestNames.forEach(guests => {
+    console.log(`Asallam o alaikum ! ${guests} would you like to come to my house for dinner tommorow ?`)
+});
+
