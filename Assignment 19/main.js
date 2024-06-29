7e3ec28f71f550574df08ea5067b98fe02d97256
Syed_Daniyal-14 <syeddaniyalakhtar4810@gmail.com>
2024-06-29 12:38:46 +0500

    first commit

---
+++ Assignment 19/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//Assignment : 19, Dinner guests
+//continue with the array of exercise 14
+let guestNames = ["Aliyan", "Rafay", "Muhammad", "Kashan", "Musab", "Maaz"];
+//print the message
+console.log(`I have invited ${guestNames.length} people to the dinner.`);
---
+++ Assignment 19/main.ts
@@ -0,0 +1,8 @@
+//Assignment : 19, Dinner guests
+
+//continue with the array of exercise 14
+
+let guestNames: string[] = ["Aliyan" , "Rafay" , "Muhammad" , "Kashan" , "Musab" , "Maaz"];
+
+//print the message
+console.log(`I have invited ${guestNames.length} people to the dinner.`)
