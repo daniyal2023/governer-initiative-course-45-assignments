f3109ac1b06cbe3d500aad86ffcef9aa269aaacb
Syed_Daniyal-14 <syeddaniyalakhtar4810@gmail.com>
2024-06-29 16:32:04 +0500

    first commit

---
+++ Assignment 20/main.js
@@ -0,0 +1,9 @@
+"use strict";
+//Assignment : 20 
+//create an array of cities
+let cities = ["Karachi", "Islamabad", "Lahore", "Quetta", "Kashmir", "Peshawar"];
+//print the list of cities
+console.log("List of cities :");
+cities.forEach(famousCities => {
+    console.log(famousCities);
+});
---
+++ Assignment 20/main.ts
@@ -0,0 +1,10 @@
+//Assignment : 20 
+
+//create an array of cities
+let cities: string[] = ["Karachi" , "Islamabad" , "Lahore" , "Quetta" , "Kashmir" , "Peshawar"];
+
+//print the list of cities
+console.log("List of cities :");
+cities.forEach(famousCities => {
+    console.log(famousCities)
+});
