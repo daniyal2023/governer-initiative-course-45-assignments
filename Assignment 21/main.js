bcad43e2f204d211ff1d69c7c520fb772ed8ed94
Syed_Daniyal-14 <syeddaniyalakhtar4810@gmail.com>
2024-06-30 21:28:35 +0500

    First commit

---
+++ Assignment 21/main.js
@@ -0,0 +1,11 @@
+"use strict";
+//Assignment : 21
+//Create an object which stores infromation about a car
+let car = {
+    name: "Honda Civic",
+    price: 35000,
+    model: 2023,
+    condition: "Brand new"
+};
+//Print the infromation stored in object
+console.log("Car infromation :", car);
---
+++ Assignment 21/main.ts
@@ -0,0 +1,17 @@
+//Assignment : 21
+
+//Create an object which stores infromation about a car
+let car : {
+    name : string
+    price : number
+    model : number
+    condition : string
+} = {
+    name : "Honda Civic",
+    price : 35000,
+    model : 2023,
+    condition : "Brand new"
+};
+
+//Print the infromation stored in object
+console.log("Car infromation :" , car)
