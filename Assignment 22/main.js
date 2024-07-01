bb7a6cfdc9edcead09e5a1cd8399e249403bf6af
Syed_Daniyal-14 <syeddaniyalakhtar4810@gmail.com>
2024-07-01 11:14:28 +0500

    first commit

---
+++ Assignment 22/main.js
@@ -0,0 +1,8 @@
+"use strict";
+//Assignment : 21, Intentional error
+//craete an array
+let fruits = ["Apple", "Banana", "Mango", "Cherry"];
+//make a index error
+console.log(fruits[4]);
+//correct the index error
+console.log(fruits[2]);
---
+++ Assignment 22/main.ts
@@ -0,0 +1,10 @@
+//Assignment : 21, Intentional error
+
+//craete an array
+let fruits : string[] = ["Apple", "Banana", "Mango", "Cherry"];
+
+//make a index error
+console.log(fruits[4]);
+
+//correct the index error
+console.log(fruits[2]);
