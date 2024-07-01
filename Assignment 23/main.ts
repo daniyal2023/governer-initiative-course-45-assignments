1e23c89d68d346f04d59fe39d345e670a2b1ff03
Syed_Daniyal-14 <syeddaniyalakhtar4810@gmail.com>
2024-07-01 12:21:34 +0500

    first commit

---
+++ Assignment 23/main.ts
@@ -0,0 +1,43 @@
+//Assignment : 23, Conditional test
+
+let bike : string = "Yamaha YBR 125";
+
+//Test no.1
+console.log("Is bike == 'Yamaha YBR 125' ? I predict True.");
+console.log(bike == "Yamaha YBR 125"); 
+
+//Test no.2
+console.log("Is bike == 'Suzuki 150' ? I predict False.");
+console.log(bike == "Suzuki 150"); 
+
+//Test no.3
+console.log("Is bike === 'Yamaha YBR 125' ? I predict True.");
+console.log(bike === "Yamaha YBR 125");
+
+//Test no.4
+console.log("Is bike.length < 10 ? I predict Flase.");
+console.log(bike.length < 10 );
+
+//Test no.5
+console.log("Is bike.length > 10 ? I predict True.");
+console.log(bike.length > 10 );
+
+//Test no.6
+console.log("Is bike != 'Yamaha YBR 125' ? I predict False");
+console.log(bike != "Yamaha YBR 125");
+
+//Test no.7
+console.log("Is bike != 'Suzuki 150' ? I predict True.");
+console.log(bike != "Suzuki 150");
+
+//Test no.8
+console.log("Is bike !== 'Yamaha YBR 125' ? I predict False");
+console.log(bike !== "Yamaha YBR 125");
+
+//Test no.9
+console.log("Is bike.length >= 14 ? I predict True.");
+console.log(bike.length >= 14 );
+
+//Test no.10
+console.log("Is bike.length <= 10 ? I predict Flase.");
+console.log(bike.length <= 10 );
