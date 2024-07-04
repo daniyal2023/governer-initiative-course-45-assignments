573f6783a3444c7102cec73c2bf455d57a167391
Syed_Daniyal-14 <syeddaniyalakhtar4810@gmail.com>
2024-07-04 21:24:38 +0500

    first commit

---
+++ Assignment 27/main.ts
@@ -0,0 +1,37 @@
+//Assignment 27, Alien color 3
+
+// green alien version
+let alienColor: string = "green";
+
+//create if-else chain
+if (alienColor === "green"){
+    console.log("Player earned 5 points for shooting the alien.");
+} else if (alienColor === "yellow"){
+    console.log("player earned 10 points for shooting the alien.");
+} else if (alienColor === "red"){
+    console.log("player earned 15 points for shooting the alien.");
+};
+
+// yellow alien version
+alienColor = "yellow";
+
+//create if-else chain
+if (alienColor === "green"){
+    console.log("Player earned 5 points for shooting the alien.");
+} else if (alienColor === "yellow"){
+    console.log("player earned 10 points for shooting the alien.");
+} else if (alienColor === "red"){
+    console.log("player earned 15 points for shooting the alien.");
+};
+
+// red alien version 
+alienColor = "red";
+
+//create if-else chain
+if (alienColor === "green"){
+    console.log("Player earned 5 points for shooting the alien.");
+} else if (alienColor === "yellow"){
+    console.log("player earned 10 points for shooting the alien.");
+} else if (alienColor === "red"){
+    console.log("player earned 15 points for shooting the alien.");
+};
