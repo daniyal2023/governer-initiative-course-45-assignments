94407d6fc50df76d9e8682bc1a8ffd1477d4e868
Syed_Daniyal-14 <syeddaniyalakhtar4810@gmail.com>
2024-07-04 23:28:25 +0500

    first commit

---
+++ Assignment 28/main.ts
@@ -0,0 +1,19 @@
+//Assignment 28, Stages of life
+
+//create a variable of age
+let personAge : number = 12;
+
+//create if-else chain
+if (personAge < 2){
+    console.log("person is a baby.");
+} else if (personAge >= 2 && personAge < 4){
+    console.log("person is a toddler.");
+} else if (personAge >= 4 && personAge < 13){
+    console.log("person is a kid");
+} else if (personAge >= 13 && personAge < 20){
+    console.log("person is a teenager.");
+} else if (personAge >= 20 && personAge < 65){
+    console.log("person is a adult.");
+} else if (personAge >= 65){
+    console.log("person is a elder");
+};
