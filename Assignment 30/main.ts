1f0b98abdd2eb19d00cc0a5831f45ae017676329
Syed_Daniyal-14 <syeddaniyalakhtar4810@gmail.com>
2024-07-05 00:41:31 +0500

    first commit

---
+++ Assignment 30/main.ts
@@ -0,0 +1,12 @@
+// Assignment 30, Hello Admin 
+
+// Make a array of five or more usernames
+let usernames : string[] = ["Daniyal", "Admin" , "Muneeb" , "Rayan" , "Kashan"];
+
+usernames.forEach(username => {
+    if (username === "Admin"){
+        console.log("Hello Admin, would you like to see a status report?");
+    } else {
+        console.log(`Hello ${username}, thank you for logging in again.`);
+    };
+});
