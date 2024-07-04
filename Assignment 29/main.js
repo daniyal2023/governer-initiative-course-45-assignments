0c914def5a07d250f4cfad39a1adbbe48448be7f
Syed_Daniyal-14 <syeddaniyalakhtar4810@gmail.com>
2024-07-05 00:16:29 +0500

    first commit

---
+++ Assignment 29/main.js
@@ -0,0 +1,25 @@
+"use strict";
+//Assignment 29, Favourite fruit
+// Make a array of your three favorite fruits and call it favorite_fruits.
+let favorite_fruits = ["Apple", "Mango", "Banana", "Grapes", "Watermelon"];
+// Write 5 if statements
+if (favorite_fruits[0] === "Apple") {
+    console.log("You really like " + favorite_fruits[0] + "!");
+}
+;
+if (favorite_fruits[1] === "Mango") {
+    console.log(`You really like ${favorite_fruits[1]}!`);
+}
+;
+if (favorite_fruits[2] === "Banana") {
+    console.log(`You really like ${favorite_fruits[2]}!`);
+}
+;
+if (favorite_fruits[3] === "Grapes") {
+    console.log(`You really like ${favorite_fruits[3]}!`);
+}
+;
+if (favorite_fruits[4] === "Watermelon") {
+    console.log(`You really like ${favorite_fruits[4]}!`);
+}
+;
