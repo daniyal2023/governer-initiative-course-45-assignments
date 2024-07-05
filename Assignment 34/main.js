d9c139ba7bf936c98efe6620556e6ee48f33715b
Syed_Daniyal-14 <syeddaniyalakhtar4810@gmail.com>
2024-07-05 21:45:10 +0500

    first commit

---
+++ Assignment 34/main.js
@@ -0,0 +1,16 @@
+"use strict";
+//Assignment 34, Pizzas
+//Store the pizza names in a array
+let pizzaFlavor = ["Chicken fajita pizza", "Ranch Piazza", "Smooky BBQ pizza"];
+//craete a loop that prints the name of pizza
+for (let i = 0; i < pizzaFlavor.length; i++) {
+    console.log(pizzaFlavor[i]);
+}
+;
+//Modify loop to print a sentence instead of name of flavor
+for (let i = 0; i < pizzaFlavor.length; i++) {
+    console.log(`I really like ${pizzaFlavor[i]}!`);
+}
+;
+//print the statement about how much you like pizza ?
+console.log("\n I really like pizza so much because of thier creamy,cheesy and spicy flavours. These are my favorite flavours. There are so much toppings on a pizza and we need add toppings of our choice. We can also try some fusion with flavours. I really love pizza so much.");
