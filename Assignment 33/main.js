62da9ab98b1f17b113619df0ed2adab7aef4dba6
Syed_Daniyal-14 <syeddaniyalakhtar4810@gmail.com>
2024-07-05 19:05:59 +0500

    first commit

---
+++ Assignment 33/main.js
@@ -0,0 +1,19 @@
+"use strict";
+//Assignmnet 33, Ordinal Numbers
+//• Store the numbers 1 through 9 in a array.
+let ordinalNunbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// • Loop through the array.
+// • Use an if-else chain inside the loop to print the proper ordinal ending for each number. Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", and each result should be on a separate line.
+ordinalNunbers.forEach(numbers => {
+    let ordinalEnding = "th";
+    if (numbers === 1) {
+        ordinalEnding = "st";
+    }
+    else if (numbers === 2) {
+        ordinalEnding = "nd";
+    }
+    else if (numbers === 3) {
+        ordinalEnding = "rd";
+    }
+    console.log(`${numbers}${ordinalEnding}`);
+});
