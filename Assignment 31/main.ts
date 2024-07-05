5f21ecbf587aff675da8baf810e71507567e1614
Syed_Daniyal-14 <syeddaniyalakhtar4810@gmail.com>
2024-07-05 17:10:18 +0500

    first commit

---
+++ Assignment 31/main.ts
@@ -0,0 +1,13 @@
+//Assignment 31, No Users
+
+//Continue with the array of exercise 30 and remove all the users from it
+
+let usernames : string[] = [];
+
+//create if else statement
+ if (usernames.length === 0){
+     console.log("We need to find some users!");
+ } else {
+    console.log(`Hello ${usernames}, thank you for logging in again.`);
+ };
+
