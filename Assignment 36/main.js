f558ff4b9794842bdcccb6c031d5c2ac0838c6a3
Syed_Daniyal-14 <syeddaniyalakhtar4810@gmail.com>
2024-07-06 12:27:46 +0500

    first commit

---
+++ Assignment 36/main.js
@@ -0,0 +1,8 @@
+"use strict";
+//Assignment 36, T-shirt
+//Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it.
+function make_shirt(size, message) {
+    console.log(`Making ${size} size shirt with the message "${message}" on it.`);
+}
+//call the function
+make_shirt("large", "Coding is life");
---
+++ Assignment 36/main.ts
@@ -0,0 +1,9 @@
+//Assignment 36, T-shirt
+
+//Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it.
+function make_shirt(size:string , message:string){
+    console.log(`Making ${size} size shirt with the message "${message}" on it.`)
+}
+
+//call the function
+make_shirt("large" , "Coding is life");
